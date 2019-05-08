16ce8a20e80b4273eafaae15bcc59fa3cfd1cdc0
dustintktran <dustintktran@gmail.com>
2019-05-08 11:34:27 -0700

    Second Commit Client Startup
    Client side correctly connected to React Nothing substantial added yet

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return <div>THIS IS IN THE APP CLASS</div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"), console.log('INSIDE REACT RENDERING'));
